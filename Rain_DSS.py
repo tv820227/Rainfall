20c717f18e7da900c8078a1ba90e6f177dbfbc42
tv820227 <tv820227@gmail.com>
2018-05-03 17:06:58 +0800

    Out put all data

--- Rain_DSS.py
+++ Rain_DSS.py
@@ -45,16 +45,19 @@ with open('ShortTermPercentage.txt') as series:
             per_s.append(float(line.split()[1]))
 
 
+rainfall = {}
 # 計算短延時時間序列
-for j in range(6):
-    for i in range(6):
+for j in range(6): # j = index of duration
+    for i in range(6): # i = index of period
         I = a[i]/(d[j]*60.0 + b[i])**c[i] # 計算總降雨強度
         V = I*d[j] # 計算單位時間內的降雨量
+        raintype = str(d[j])+"hr_"+str(p[i])+"year"
         if d[j] < 6:
-            rainfall = [round(V*k/100.0,2) for k in per_s]
+            rainfall[raintype] = [round(V*k/100.0,2) for k in per_s]
         else:
-            rainfall = [round(V*k/100.0,2) for k in per_l]
+            rainfall[raintype] = [round(V*k/100.0,2) for k in per_l]
         
+#%%
 
 # 輸出計算之時間序列
 book = xlwt.Workbook()
@@ -70,6 +73,7 @@ for i in first_col:
 #%%
 # create time series
 time_series = []
+time_series_s = {}
 for h in d:
     total_time = h * 60
     if h > 3:
@@ -78,58 +82,40 @@ for h in d:
         n_interval = 12
     time_step = total_time/n_interval
     
+    time_temp = []
     for i in range(n_interval):
         time = i*time_step
         hour = "0"+str(int(time//60))
         minute = "0"+str(int(time%60))
         date_time = "01Jan2000 "+hour[-2:]+minute[-2:]
+        time_temp.append(date_time)
         if date_time not in time_series:
             time_series.append(date_time)
     sorted(time_series)
-print(time_series)
-    
+    time_series_s[h] = time_temp
 
+# output time series into excel
+cnt = 0
+for i in time_series:
+    sheet1.write(7+cnt,0,cnt+1)
+    sheet1.write(7+cnt,1,i)
+    cnt += 1
 
 # 輸出各個降雨的時間序列
 # y = year, h = hour
-# cnt = 0
-# for h in range(6):
-#     for y in range(6):
-#         col = cnt + h + y
-#         sheet1.write(0,col,h+"HR")
-#         sheet1.write(1,col,y+"YEAR")
-#         sheet1.write(5,col,"mm")
-#         sheet1.write(6,col,"INST-VAL")
+cnt = 0
+for h in range(6):
+    for y in range(6):
+        col = 2 + cnt
+        sheet1.write(0,col,str(d[h])+"HR")
+        sheet1.write(1,col,str(p[y])+"YEAR")
+        sheet1.write(5,col,"mm")
+        sheet1.write(6,col,"INST-VAL")
+        rain_key = str(d[h])+"hr_"+str(p[y])+"year"
+        rain = rainfall[rain_key]
+        for i in range(len(rain)):
+            row = 7+time_series.index(time_series_s[d[h]][i])
+            sheet1.write(row,col,rain[i]) 
+        cnt += 1
 
 book.save("C:\\TV\\model\\Hou_Cun_Zun1\\HMS_rainfall.xls")
-
-
-        # out = open('.\\Dat\\%ihr_%iyear.dat'%(d[j],p[i]),'w')
-        # out.write(';Rainfall Data for Gage G1\n')
-        
-        # time_step = int(d[j]*60/len(rainfall))
-        # h = 0
-        # m = 0
-        # for l in rainfall:
-        #     # 判斷時間是否需要進位
-        #     if m >= 60:
-        #         h += 1
-        #         m -= 60
-            
-        #     # 判斷小時是不是兩個數字，如果只有一個在前面補0
-        #     if len(str(h)) < 2:
-        #         out_h = '0' + str(h)
-        #     else:
-        #         out_h = str(h)
-            
-        #     # 判斷分鐘是不是兩個數字，如果只有一個在前面補0
-        #     if len(str(m)) < 2:
-        #         out_m = '0' + str(m)
-        #     else:
-        #         out_m = str(m)
-            
-        #     # 欲輸出之時間格式
-        #     time_series = out_h+':'+out_m
-        #     out.write(time_series+'\t'+str(l)+'\n')
-        #     m += time_step
-        # out.close()
