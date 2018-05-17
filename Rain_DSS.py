27ec3f96eccd1ff9095acb5ad4da59efb4d266af
tv820227 <tv820227@gmail.com>
2018-05-17 18:08:39 +0800

    only change label-column F

--- Rain_DSS.py
+++ Rain_DSS.py
@@ -116,7 +116,7 @@ for h in range(6):
         sheet1.write(1,col,str(p[y])+"YEAR")
         sheet1.write(2,col,"PRECIP-CUM")
         sheet1.write(5,col,"mm")
-        sheet1.write(6,col,"INST-CUM")
+        sheet1.write(6,col,"PER-CUM")
         
         # use key to find time series of this rainfall
         rain_key = str(d[h])+"hr_"+str(p[y])+"year"
